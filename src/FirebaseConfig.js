fc2245ad58d2f6a5e507f9e0ecfeb5850a531650
vennyvirtuoso <vijendra9753@gmail.com>
2023-10-02 02:46:06 +0530

    google-firebase-login

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDG0t33MQlhYz2h_55K9cm6o0_xU275FoE",
+  authDomain: "cold-email-writer-1e2ba.firebaseapp.com",
+  projectId: "cold-email-writer-1e2ba",
+  storageBucket: "cold-email-writer-1e2ba.appspot.com",
+  messagingSenderId: "681517354527",
+  appId: "1:681517354527:web:f36b424177667af62f8f2a",
+  measurementId: "G-4QGDWJBKPD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
---
+++ src/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { auth, provider } from "./FirebaseConfig";
+import { signInWithPopup } from "firebase/auth";
+
+function Login() {
+  const [user, setUser] = useState(null);
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="box">
+        {user ? (
+          <>
+            <button className="btn btn-secondary btn-md" onClick={handleLogout}>
+              LOGOUT
+            </button>
+            <h3>Welcome {user.displayName}</h3>
+            <p>{user.email}</p>
+            <div className="photo">
+              <img src={user.photoURL} alt="dp" referrerPolicy="no-referrer" />
+            </div>
+          </>
+        ) : (
+          <button
+            className="btn btn-danger btn-md"
+            onClick={handleGoogleSignIn}
+          >
+            Sign In With Google
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
